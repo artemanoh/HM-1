42f6ccbbbede5ad40361e8d9c2d231439b37a4d4
artem1214 <61391756+artemanoh@users.noreply.github.com>
2024-09-17 18:01:13 +0300

    :)

---
+++ index.js
@@ -0,0 +1,36 @@
+const age = 15;
+console.log(age);
+
+const name = "Artem";
+console.log(name);
+
+const isStudent = true;
+console.log(isStudent);
+
+const myString = "Її нажаль немає";
+console.log(myString);
+
+let myNumber = "3";
+myNumber = myNumber + 10;
+console.log(myNumber)
+
+const myNull = "null"
+console.log(myNull)
+
+const glostName = prompt("Яке ваше ім'я?")
+console.log(glostName)
+
+const isConfirm = confirm("Підтвердіть дію");
+if (isConfirm) {
+  console.log("Дякую за підтвердження!");
+} else {
+  console.log("Дію відмінено!");
+}
+
+const message = alert("Підтвердіть видалення свого аккаунта");
+const isTransfering = confirm("Чи впевнені ви у цій дії");
+if (isConfirm) {
+  console.log("Дякую за підтвердження!");
+} else {
+  console.log("Дію відмінено!");
+}
